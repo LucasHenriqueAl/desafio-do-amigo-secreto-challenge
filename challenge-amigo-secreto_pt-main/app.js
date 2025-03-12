6cff79a3dba890c2685db336a9c98a896277dbf2
Lucas Henrique <lucas.henricruz@gmail.com>
2025-03-12 19:19:11 -0300

    add função add amigo

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,21 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+    //Captura o valor do campo de entrada
+    let entrada = document.getElementById("amigo");
+    let nome = input.value.trim();
+
+    //validação da entrada
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+     // Atualiza o array de amigos
+     amigos.push(nome);
+
+     // Limpa o campo de entrada
+    input.value = "";
+}
